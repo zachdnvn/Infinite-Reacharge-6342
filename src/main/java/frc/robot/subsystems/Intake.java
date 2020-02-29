4ef6ce8d8ce928c35d078b8290d8d7009956a763
Caleb Beere <cbeere@uoguelph.ca>
2020-02-28 19:06:51 -0500

    Added intake toggling

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,23 +8,41 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
-
 public class Intake extends SubsystemBase {
-  WPI_TalonSRX intakeTalon = new WPI_TalonSRX(14);
+  WPI_TalonSRX intakeTalon;
 
-  
-  public void IntakeIn(){
+  DoubleSolenoid m_intake_Solenoid;
+  Boolean intake_SolenoidState = true; // SUBJECT TO CHANGE BASED ON STARTING POSITION
+
+  public Intake() {
+    intakeTalon = new WPI_TalonSRX(14);
+    m_intake_Solenoid = new DoubleSolenoid(13, 14); // ID'S ARE SUBJECT TO CHANGE
+  }
+
+  public void changeIntake() {
+    intake_SolenoidState = !intake_SolenoidState;
+    if (intake_SolenoidState == true) {
+      m_intake_Solenoid.set(Value.kReverse);
+    } else {
+      m_intake_Solenoid.set(Value.kForward);
+    }
+  }
+
+  public void IntakeIn() {
     intakeTalon.set(IntakeConstants.rollerspeed);
   }
 
-  public void IntakeOut(){
+  public void IntakeOut() {
     intakeTalon.set(IntakeConstants.rollerspeed * -1);
   }
 
-  public void IntakeStop(){
+  public void IntakeStop() {
     intakeTalon.set(0);
   }
 
